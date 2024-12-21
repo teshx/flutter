cc291bfc908a47fb0bff05795d38d390b02a1ea0
teshager habtie <teshie00001@gmail.com>
2024-12-21 14:33:43 -0600

    task 3

---
+++ flutter_application_1/lib/task3.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const CsItSwx());
+// }
+
+// class CsItSwx extends StatelessWidget {
+//   const CsItSwx({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Column(
+//             children: [
+//               const Text('Hello,cs,it and se',textDirection: TextDirection.ltr,),
+//               ElevatedButton(onPressed:(){}, child: const Text('cs,it,so'))
+//             ]
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
