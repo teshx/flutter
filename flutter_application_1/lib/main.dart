a1a2da8e654796ef503070ef66e879d85aea1316
teshager habtie <teshie00001@gmail.com>
2024-12-21 13:46:17 -0600

    this is state less

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'Hello ,CS,IT,and SW',
-        textDirection: TextDirection.ltr,
+  runApp(const CsItSwx());
+}
+
+class CsItSwx extends StatelessWidget {
+  const CsItSwx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+               Text(
+                'Hello, CS, IT and SE ',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ), 
+
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  textStyle:  TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ), 
+ 
+                child: Text('cs,it,so'),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
